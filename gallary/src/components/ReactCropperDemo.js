59cef91989ab2227a0b849c1a5d52ee542dd714b
Srikanth Bathi <sbathi@yodle.com>
2020-02-22 14:37:04 +0530

    final

--- gallary/src/components/ReactCropperDemo.js
+++ gallary/src/components/ReactCropperDemo.js
@@ -7,16 +7,32 @@ class ReactCropperDemo extends React.Component{
     constructor(props){
         super(props);
         this.cropImg = new React.createRef();
+        this.canvas = new React.createRef();
+        this.img = new React.createRef();
         this.state =
         {
             src:process.env.PUBLIC_URL+"/test.png",
             prev:""
         }
     }
-    cropItem(){
+    cropItem(crop){
         const dataUrl = "";
-        console.log(this.cropImg.current.cropper)
-        console.log(dataUrl);
+                console.log(this.cropImg.cropper.getImageData())
+                console.log(this.cropImg.cropper.getCropBoxData().left)
+                console.log(this.cropImg.cropper.getData())
+                console.log(this.canvas);
+                var img = new Image();
+                 img.src = this.state.src;
+                 img.onload = ()=>{
+                     console.log("loaded")
+                     
+                    var ctx = this.canvas.current.getContext('2d');
+                    ctx.clearRect(0,0,250,290)
+                    console.log(ctx)
+                    ctx.drawImage(img,this.cropImg.cropper.getData().x,this.cropImg.cropper.getData().y,974,1278,0,0,250,290);
+                 }
+                
+
         this.setState({
             prev:dataUrl
         })
@@ -24,20 +40,21 @@ class ReactCropperDemo extends React.Component{
     render(){
         return(<div>
             <Cropper src={this.state.src}
-            ref={this.cropImg}
+            ref={cropper => { this.cropImg = cropper; }}
             aspectRatio={21/25}
             crop = {this.cropItem.bind(this)}
             style={{height: 400, width: '50%'}}
             rotateTo={45}
             rotatable={true}
             responsive={true}
-            guides={false}
-            
+            guides={true}
+            autoCrop={true}
+            background={false}
             >
 
             </Cropper>
-
-            <img src = {this.state.prev}></img>
+            <canvas id = "prev" ref ={this.canvas} width="250" height="290"></canvas>
+            <img ref = {this.img} src = {this.state.src}></img>
             </div>
         )
     }
---
+++ gallary/src/components/ReactImageCrop.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import ReactCrop from 'react-image-crop';
+import 'react-image-crop/lib/ReactCrop.scss';
+
+export default class ReactImageCrop extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
